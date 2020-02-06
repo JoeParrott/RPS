3fed104d884c59942be06739ccad667606bf6860
JoeParrott <JosephParrott1@outlook.com>
2020-02-06 19:25:50 +0000

    adding compare function with if else usage

--- RPS1.js
+++ RPS1.js
@@ -4,23 +4,65 @@ console.log("Welcome to Rock Paper Scissors v0.1");
 
 // Player choice- use the readline module
 
-const readLine = require("readLine").createInterface({
+const readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stout
 });
 
-function handleUserResponse (response) {
+readline.question("Please choose rock, paper or scissors\n", userResponse)
 
-// convert choice to number
+    function userResponse (response) {
 
-// terminal choses num math.method
+    // terminal chooses math.method
 
-// compare nums, else if
+        let termOpt = ["rock", "paper", "scissors"];
+        let termChoice = termOpt[Math.floor(Math.random() * 3)];
 
+// compare, else if
+
+        const compare = (userResponse, termChoice) => {
+            if (userResponse === termChoice) {
+                console.log("This is a tie.");
+                return;
+    }
+            if (userResponse === "rock") {
+                if (termChoice === "paper") {
+                    console.log("You lost.");
+                    return;
+        }
+                else {
+                    console.log("You win.");
+                    return;
+        }
+    }
+            if (userResponse === "paper") {
+                if (termChoice === "scissors") {
+                    console.log("You lost.");
+                    return;
+        }
+                else {
+                    console.log("You win.");
+                    return;
+        }
+    }
+            if (userResponse === "scissors") {
+                if (termChoice === "rock") {
+                    console.log("You lost.");
+                    return;
+        }
+                else {
+                    console.log("You win.");
+                    return;
+        }
+    }
+}
 // output winner
 
 }
 
+
+
+
 // loop to start?
 
-readLine.question("Please choose rock, paper or scissors\n", handleUserResponse);
+
