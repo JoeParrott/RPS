70e61123ee7563c86ad0660b71f3ea66369c447e
JoeParrott <JosephParrott1@outlook.com>
2020-02-06 02:33:56 +0000

    added prompt using readLine mod

--- RPS1.js
+++ RPS1.js
@@ -1,1 +1,26 @@
-console.log("test");
+// Intro
+
+console.log("Welcome to Rock Paper Scissors v0.1");
+
+// Player choice- use the readline module
+
+const readLine = require("readLine").createInterface({
+    input: process.stdin,
+    output: process.stout
+});
+
+function handleUserResponse (response) {
+
+// convert choice to number
+
+// terminal choses num math.method
+
+// compare nums, else if
+
+// output winner
+
+}
+
+// loop to start?
+
+readLine.question("Please choose rock, paper or scissors\n", handleUserResponse);
