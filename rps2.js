b589f8088b9873117ffde670808945c4e74479fc
JoeParrott <JosephParrott1@outlook.com>
2020-02-07 03:26:29 +0000

    rps2 taking form, still struggling with scope

--- rps2.js
+++ rps2.js
@@ -6,11 +6,48 @@ const readline = require('readline').createInterface({
 });
 
 function userResponse (response) {
+                                                                // is the input valid?
+    if (response === "rock" || "paper" || "scissors") {
+        let termOpt = ["rock", "paper", "scissors"];
+        let termChoice = termOpt [Math.floor(Math.random() * 3)];
+        console.log("Terminal chose: " + termChoice)
+                                                                // why is termChoice not being read?
 
-    console.log(response);
-
-// everything in here!
-
+        const compare = (response, termChoice) => {
+            if (response === termChoice) {
+                console.log("This is a tie.");
+            }
+            else if (response === "rock") {
+                if (termChoice === "paper") {
+                    console.log("You lost.");
+            }
+                else {
+                    console.log("You win.");
+            }
+        }
+            else if (response === "paper") {
+                if (termChoice === "scissors") {
+                    console.log("You lost.");
+            }
+                else {
+                    console.log("You win.");
+            }
+        }
+            else if (response === "scissors") {
+                if (termChoice === "rock") {
+                    console.log("You lost.");
+            }
+                else {
+                    console.log("You win.");
+            }
+        }
+        else {
+        console.log("Please choose again.")
+        }
+    }
+    let result = compare(response, termChoice);
+    console.log(result);
 }
-
-readline.question("Please choose rock, paper or scissors:\n", userResponse)
+    readline.close();
+}
+readline.question("Please choose rock, paper or scissors:\n", userResponse);
